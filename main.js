69c2511844d70359ff8c73f2f3582b7d461208dd
stefanovidmar2005 <stefanovidmar@gmail.com>
2023-09-09 15:17:13 +0300

    mobile / desktop responseive

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ accordions.forEach((accordion, index) => {
   const html = `
     <div class="accordion__box" style="background-color:${
       backgroundColors[index]
-    }; border-left: 5px solid ${borderColors[index]} ;" data-id=${index + 1}>
+    }; border-left: 4px solid ${borderColors[index]} ;" data-id=${index + 1}>
               <div class="accordion__box-headings">
                <h3 class="accordion__box-heading">${accordion.title}</h3>
                <div class="plus">
@@ -24,7 +24,21 @@ accordions.forEach((accordion, index) => {
                <span class="line line-horizontal"></span>
              </div>
                 </div>
-                <p>${accordion.text}</p>
+                <p class='accordion__description'>${accordion.text}</p>
                 </div>`;
   accordionContainer.insertAdjacentHTML("beforeend", html);
+});
+
+// Event Delegation
+
+accordionContainer.addEventListener("click", (e) => {
+  e.preventDefault();
+  const target = e.target.closest(".plus");
+  if (!target) return;
+  const parent = target.parentElement.parentElement;
+  const paragraphEl = parent.querySelector(".accordion__description");
+  const header = parent.querySelector(".accordion__box-heading");
+  target.classList.toggle("active-accordion");
+  paragraphEl.classList.toggle("active-description");
+  header.classList.toggle("dark");
 });
